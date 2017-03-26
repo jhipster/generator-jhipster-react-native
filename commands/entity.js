dc215296648a5e3eebf38e37caf711eec3add261
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2017-03-26 13:04:32 -0700

    set up saga/redux generation and wiring for entities

--- commands/entity.js
+++ commands/entity.js
@@ -3,7 +3,7 @@
 module.exports = async function (context) {
     // grab some features
     const { parameters, strings, print, ignite } = context
-    const { pascalCase, isBlank } = strings
+    const { pascalCase, snakeCase, camelCase, isBlank } = strings
     const config = ignite.loadIgniteConfig()
     const { tests } = config
 
@@ -14,40 +14,83 @@ module.exports = async function (context) {
         return
     }
 
+    // read some configuration
+    const name = pascalCase(parameters.first)
+    const props = { name }
+
+    const jhipsterApiFilePath = `${process.cwd()}/App/Services/JhipsterApi.js`
+    const reduxIndexFilePath = `${process.cwd()}/App/Redux/index.js`
+    const sagaIndexFilePath = `${process.cwd()}/App/Sagas/index.js`
+
+
+    // REDUX AND SAGA SECTION
+    const apiMethods = `
+  const update${props.name} = () => api.put('api/${camelCase(props.name)}s')
+  const get${props.name}s = () => api.get('api/${camelCase(props.name)}s')
+  const get${props.name} = (${camelCase(props.name)}Id) => api.get('api/${camelCase(props.name)}s/' + ${camelCase(props.name)}Id)
+  const delete${props.name} = (${camelCase(props.name)}Id) => api.delete('api/${camelCase(props.name)}s/' + ${camelCase(props.name)}Id)`
+
+    const apiMethodsExport = `
+    update${props.name},
+    get${props.name}s,
+    get${props.name},
+    delete${props.name},`
+
     // add methods to api
-    // ignite-jhipster-api-method-needle
-    // ignite-jhipster-api-export-needle
-
-    // const updateFoo = () => api.put('api/foos')
-    // const getFoos = () => api.get('api/foos')
-    // const getFoo = (fooId) => api.get('api/foos/' + fooId)
-    // const deleteFoo = (fooId) => api.delete('api/foos/' + fooId)
-
-    // updateFoo,
-    // getFoos,
-    // getFoo,
-    // deleteFoo,
-
-    // generate a saga for the entity
-
-    // generate a redux for the entity
+    ignite.patchInFile(jhipsterApiFilePath, {
+        before: 'ignite-jhipster-api-method-needle',
+        insert: apiMethods,
+        match: apiMethods
+    })
+    ignite.patchInFile(jhipsterApiFilePath, {
+        before: 'ignite-jhipster-api-export-needle',
+        insert: apiMethodsExport,
+        match: apiMethodsExport
+    })
 
     // import redux in redux/index.js
-    // ignite-jhipster-redux-store-import-needle
-    // foos: require('./FooRedux').reducer,
+    ignite.patchInFile(reduxIndexFilePath, {
+        before: 'ignite-jhipster-redux-store-import-needle',
+        insert: `${camelCase(props.name)}s: require('./${props.name}Redux').reducer`,
+        match: `${camelCase(props.name)}s: require('./${props.name}Redux').reducer`
+    })
 
     // import saga/redux in sagas/index.js
-    // ignite-jhipster-saga-redux-import-needle
-    // import { FooTypes } from '../Redux/FooRedux'
+    ignite.patchInFile(sagaIndexFilePath, {
+        before: 'ignite-jhipster-saga-redux-import-needle',
+        insert: `import { ${props.name}Types } from '../Redux/${props.name}Redux'`,
+        match: `import { ${props.name}Types } from '../Redux/${props.name}Redux'`
+    })
+    ignite.patchInFile(sagaIndexFilePath, {
+        before: 'ignite-jhipster-saga-method-import-needle',
+        insert: `import { get${props.name}, get${props.name}s, update${props.name}, delete${props.name} } from './${props.name}Sagas'`,
+        match: `import { get${props.name}, get${props.name}s, update${props.name}, delete${props.name} } from './${props.name}Sagas'`
+    })
 
-    // ignite-jhipster-saga-method-import-needle
-    // import { getFoo, getFoos, updateFoo, deleteFoo } from './FooSagas'
+    const sagaConnections = `
+    takeLatest(${props.name}Types.${snakeCase(props.name).toUpperCase()}_REQUEST, get${props.name}, jhipsterApi),
+    takeLatest(${props.name}Types.${snakeCase(props.name).toUpperCase()}_ALL_REQUEST, get${props.name}s, jhipsterApi),
+    takeLatest(${props.name}Types.${snakeCase(props.name).toUpperCase()}_UPDATE_REQUEST, update${props.name}, jhipsterApi),
+    takeLatest(${props.name}Types.${snakeCase(props.name).toUpperCase()}_DELETE_REQUEST, delete${props.name}, jhipsterApi),`
 
-    // ignite-jhipster-saga-redux-connect-needle
-    // takeLatest(FooTypes.FOO_REQUEST, getFoo, jhipsterApi),
-    // takeLatest(FooTypes.FOO_ALL_REQUEST, getFoos, jhipsterApi),
-    // takeLatest(FooTypes.FOO_UPDATE_REQUEST, updateFoo, jhipsterApi),
-    // takeLatest(FooTypes.FOO_DELETE_REQUEST, deleteFoo, jhipsterApi),
+    ignite.patchInFile(sagaIndexFilePath, {
+        before: 'ignite-jhipster-saga-redux-connect-needle',
+        insert: sagaConnections,
+        match: sagaConnections
+    })
+
+    const sagaReduxJobs = [
+        {
+            template: `saga.ejs`,
+            target: `App/Sagas/${name}Sagas.js`
+        },
+        {
+            template: `redux.ejs`,
+            target: `App/Redux/${name}Redux.js`
+        }
+    ]
+
+    await ignite.copyBatch(context, sagaReduxJobs, props)
 
 
     // generate entity listing component
@@ -68,10 +111,7 @@ module.exports = async function (context) {
 
     // add screen for entities, link to listings page
 
-    // read some configuration
-    const name = pascalCase(parameters.first)
-    const props = { name }
-    const jobs = [
+    // const jobs = [
         // component jobs
         // {
         //     template: 'component.ejs',
@@ -91,17 +131,6 @@ module.exports = async function (context) {
         //     target: `App/Containers/Styles/${name}ScreenStyle.js`
         // },
         //
-        // saga jobs
-        {
-            template: `saga.ejs`,
-            target: `App/Sagas/${name}Sagas.js`
-        },
-        // redux jobs
-        {
-            template: `redux.ejs`,
-            target: `App/Redux/${name}Redux.js`
-        },
-
         // listview jobs
         // {
         //     template: `listview.ejs`,
@@ -111,7 +140,7 @@ module.exports = async function (context) {
         //     template: `listview-style.ejs`,
         //     target: `App/Containers/Styles/${name}Style.js`
         // }
-    ]
+    // ]
 
     // if (tests) {
     //     // component tests
@@ -133,6 +162,6 @@ module.exports = async function (context) {
     //     })
     // }
 
-    await ignite.copyBatch(context, jobs, props)
+    // await ignite.copyBatch(context, jobs, props)
 
 }
