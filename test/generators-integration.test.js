cfc61d41df1c67ffa5fa8735ef00abd047588b4b
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2017-09-24 22:15:53 -0700

    add session auth generator integration test

--- test/generators-integration.test.js
+++ test/generators-integration.test.js
@@ -113,6 +113,41 @@ describe('UAA Integration Test', () => {
   })
 })
 
+describe('Session Integration Test', () => {
+  beforeAll(async () => {
+    // creates a new temp directory
+    process.chdir(tempy.directory())
+    console.log('Generating app...')
+    await execa(IGNITE, ['new', APP, '--auth-type=session', '--search-engine=false', '--dev-screens=false', '--animatable=false', '--skip-git', '--boilerplate', BOILERPLATE])
+    process.chdir(APP)
+    console.log('App generation complete!')
+  })
+
+  test('lints a fresh app', async () => {
+    console.log('Linting fresh app')
+    const lint = await execa('npm', ['-s', 'run', 'lint'])
+    expect(lint.stderr).toBe('')
+  })
+
+  test('generates two entities', async () => {
+    console.log('Generating entities...')
+    await execa(IGNITE, ['g', 'entity', 'Foo', '--jh-dir=../test'])
+    await execa(IGNITE, ['g', 'entity', 'FieldTestEntity', '--jh-dir=../test'])
+    console.log('Generated entities')
+    // t.is(jetpack.exists('App/Components/Test.js'), 'file')
+    // t.is(jetpack.exists('App/Components/Styles/TestStyle.js'), 'file')
+    const lint = await execa('npm', ['-s', 'run', 'lint'])
+    expect(lint.stderr).toBe('')
+  })
+
+  test('passes generated tests', async () => {
+    console.log('Running Tests')
+    const tests = await execa('npm', ['-s', 'run', 'test', '--', '-u'])
+    console.log('Tests Complete')
+    expect(tests.stderr).not.toMatch(/failed/)
+  })
+})
+
 describe('ElasticSearch Integration Test', () => {
   beforeAll(async () => {
     // creates a new temp directory
