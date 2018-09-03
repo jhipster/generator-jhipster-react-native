a62aeaf3fbc4c91be76e9301c0ea045596cb29c4
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2018-09-02 17:51:14 -0700

    lint, remove social code

--- boilerplate/test/spec/modules/account/password/change-passsword.sagas.spec.js
+++ boilerplate/test/spec/modules/account/password/change-passsword.sagas.spec.js
@@ -8,7 +8,7 @@ const stepper = (fn) => (mock) => fn.next(mock).value
 
 test('change password success path', () => {
   const response = FixtureAPI.changePassword({currentPassword: 'valid-password', newPassword: 'valid-password'})
-  const step = stepper(changePassword(FixtureAPI,{currentPassword: 'valid-password', newPassword: 'valid-password'}))
+  const step = stepper(changePassword(FixtureAPI, {currentPassword: 'valid-password', newPassword: 'valid-password'}))
   // Step 1: Hit the api
   step()
   // Step 2: Successful return and data!
