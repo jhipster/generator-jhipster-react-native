ac7abf8b886499a6d80566cb7034cd3eb27f64e9
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2018-09-02 17:09:37 -0700

    lint

--- boilerplate/Tests/Sagas/PasswordSagaTest.js
+++ boilerplate/Tests/Sagas/PasswordSagaTest.js
@@ -25,7 +25,7 @@ test('forgot password failure path', () => {
 
 test('change password success path', () => {
   const response = FixtureAPI.changePassword({currentPassword: 'valid-password', newPassword: 'valid-password'})
-  const step = stepper(changePassword(FixtureAPI,{currentPassword: 'valid-password', newPassword: 'valid-password'}))
+  const step = stepper(changePassword(FixtureAPI, {currentPassword: 'valid-password', newPassword: 'valid-password'}))
   // Step 1: Hit the api
   step()
   // Step 2: Successful return and data!
