0b83b072ba9a6ecaec370daf07803e78f0b87f4c
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2018-09-03 12:54:16 -0700

    re-enabled StoriesTest

--- boilerplate/App/Components/FullButton.story.js
+++ boilerplate/App/Components/FullButton.story.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import { storiesOf } from '@storybook/react-native'
 
 import FullButton from './FullButton'
--- boilerplate/App/Components/RoundedButton.story.js
+++ boilerplate/App/Components/RoundedButton.story.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import { storiesOf } from '@storybook/react-native'
 
 import RoundedButton from './RoundedButton'
