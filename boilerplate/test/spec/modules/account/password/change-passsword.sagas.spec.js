dee59b6852496f5c8a38ce2ae7f484ebfbc5689e
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2018-09-02 17:12:57 -0700

    fix change password to require current password

--- boilerplate/test/spec/modules/account/password/change-passsword.sagas.spec.js
+++ boilerplate/test/spec/modules/account/password/change-passsword.sagas.spec.js
@@ -7,8 +7,8 @@ import ChangePasswordActions from '../../../../../app/modules/account/password/c
 const stepper = (fn) => (mock) => fn.next(mock).value
 
 test('change password success path', () => {
-  const response = FixtureAPI.changePassword({password: 'valid-password'})
-  const step = stepper(changePassword(FixtureAPI, {password: 'valid-password'}))
+  const response = FixtureAPI.changePassword({currentPassword: 'valid-password', newPassword: 'valid-password'})
+  const step = stepper(changePassword(FixtureAPI,{currentPassword: 'valid-password', newPassword: 'valid-password'}))
   // Step 1: Hit the api
   step()
   // Step 2: Successful return and data!
@@ -16,8 +16,8 @@ test('change password success path', () => {
 })
 
 test('change password failure path', () => {
-  const response = FixtureAPI.changePassword({password: 'not-valid-password'})
-  const step = stepper(changePassword(FixtureAPI, {password: 'not-valid-password'}))
+  const response = FixtureAPI.changePassword({currentPassword: 'invalid-password', newPassword: 'invalid-password'})
+  const step = stepper(changePassword(FixtureAPI, {currentPassword: 'invalid-password', newPassword: 'invalid-password'}))
   // Step 1: Hit the api
   step()
   // Step 2: Successful return and data!
