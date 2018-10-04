51569c297b2d8e1b891781ffaa93cd49aa472533
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2018-10-03 23:27:43 -0700

    fix LocalDates - map to JS date on load, display as LocalDate
    related to #53

---
+++ boilerplate/App/Transforms/DateTransforms.js
@@ -0,0 +1,15 @@
+export const jsDateToLocalDate = (date) => {
+  if (!date || !date.getMonth) {
+    return ''
+  }
+  return ('0' + (date.getMonth() + 1).toString()).substr(-2) + '/' + ('0' + date.getDate().toString()).substr(-2) + '/' + (date.getFullYear().toString()).substr(2)
+}
+export const localDateToJsDate = (localDate) => {
+  const split = localDate.split('-')
+  const year = split[0]
+  const month = split[1] - 1
+  const day = split[2]
+  const d = new Date()
+  d.setUTCFullYear(year, month, day)
+  return d
+}
