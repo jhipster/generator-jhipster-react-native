32429002a67ad6e247d70441bac8dbf397d969eb
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2018-11-18 20:40:48 -0800

    patch app links in native files
    related to #56

--- src/lib/react-native-navigation.js
+++ src/lib/react-native-navigation.js
@@ -23,12 +23,36 @@ const patchReactNativeNavigation = async (context = {}, name) => {
     // quiet: true,
     directory: `${__dirname}/../../templates/react-native-navigation/`
   })
-  spinner.succeed('Set up react-native-navigation for iOS/Android')
-  await updateAndroidFiles(context)
-}
-const updateAndroidFiles = async (context) => {
-  const { ignite } = context
 
+  spinner.succeed('Set up react-native-navigation for iOS/Android')
+  await updateIosFiles(context, props)
+  await updateAndroidFiles(context, props)
+}
+const updateIosFiles = async (context, props) => {
+  const {ignite} = context
+  /*eslint-disable */
+  await ignite.patchInFile(`${process.cwd()}/ios/${props.name}/Info.plist`, {
+    before: `<key>CFBundleDisplayName</key>`,
+    insert: `
+<key>CFBundleURLTypes</key>
+	<array>
+		<dict>
+			<key>CFBundleTypeRole</key>
+			<string>Editor</string>
+			<key>CFBundleURLName</key>
+			<string>${props.name.toLowerCase()}</string>
+			<key>CFBundleURLSchemes</key>
+			<array>
+				<string>${props.name.toLowerCase()}</string>
+			</array>
+		</dict>
+	</array>`
+  })
+  /*eslint-enable */
+}
+
+const updateAndroidFiles = async (context, props) => {
+  const {ignite} = context
   // settings.gradle
   await ignite.patchInFile(`${process.cwd()}/android/settings.gradle`, {
     after: `include ':app'`,
@@ -86,9 +110,19 @@ const updateAndroidFiles = async (context) => {
   }`
   })
 
-  await ignite.patchInFile(`${process.cwd()}/android/app/build.gradle`, {
-    after: `// From node_modules`,
-    insert: `    implementation project(':react-native-navigation')`
+  // register url scheme for deep links
+  await ignite.patchInFile(`${process.cwd()}/android/app/src/main/AndroidManifest.xml`, {
+    before: `android:name=".MainActivity"`,
+    insert: `        android:launchMode="singleTask"`
+  })
+  await ignite.patchInFile(`${process.cwd()}/android/app/src/main/AndroidManifest.xml`, {
+    before: `<intent-filter>`,
+    insert: `        <intent-filter>
+            <action android:name="android.intent.action.VIEW" />
+            <category android:name="android.intent.category.DEFAULT" />
+            <category android:name="android.intent.category.BROWSABLE" />
+            <data android:scheme="${props.name.toLowerCase()}" />
+        </intent-filter>`
   })
 }
 
