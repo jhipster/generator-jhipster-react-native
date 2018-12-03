08dcd9023b8646d261265cde75680f3763dd30f6
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2018-12-02 21:29:32 -0800

    add CRUD e2e tests

--- boilerplate/e2e/utils.js
+++ boilerplate/e2e/utils.js
@@ -1,18 +1,18 @@
-navigateToLoginScreen = async () => {
+const navigateToLoginScreen = async () => {
   await expect(element(by.id('launchScreen'))).toBeVisible()
   await element(by.id('menuButton')).tap()
   await element(by.id('loginDrawerButton')).tap()
   await expect(element(by.id('loginScreenUsername'))).toBeVisible()
 }
 
-loginAsUser = async () => {
+const loginAsUser = async () => {
   await navigateToLoginScreen()
   await element(by.id('loginScreenUsername')).replaceText('user')
   await element(by.id('loginScreenPassword')).replaceText('user')
   await element(by.id('loginScreenLoginButton')).tap()
 }
 
-logout = async () => {
+const logout = async () => {
   await element(by.id('menuButton')).tap()
   await element(by.id('logoutDrawerButton')).tap()
 }
