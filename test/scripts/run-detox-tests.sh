553fc7dc7a4c28b1126c3b7b1fb37cfb223068ca
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2019-04-02 17:54:27 -0400

    improve e2e test to wait for a 200 response from the backend

--- test/scripts/run-detox-tests.sh
+++ test/scripts/run-detox-tests.sh
@@ -7,10 +7,10 @@ nohup ./mvnw &
 # wait for the backend to start
 # see https://github.com/jhipster/generator-jhipster/blob/2a803eca36f21079320c602645e13c177f6c6ea9/test-integration/scripts/24-tests-e2e.sh
 retryCount=1
-maxRetry=10
-httpUrl="http://localhost:8080"
+maxRetry=30
+httpUrl="http://localhost:8080/management/health"
 
-rep=$(curl -v "$httpUrl")
+rep=$(curl -v -f "$httpUrl")
 status=$?
 while [ "$status" -ne 0 ] && [ "$retryCount" -le "$maxRetry" ]; do
     echo "*** [$(date)] Application not reachable yet. Sleep and retry - retryCount =" $retryCount "/" $maxRetry
@@ -24,8 +24,6 @@ if [ "$status" -ne 0 ]; then
     echo "*** [$(date)] Not connected after" $retryCount " retries."
     return 1
 fi
-
-sleep 60
 
 cd ../${JHI_IGNITE_APP_NAME}
 
