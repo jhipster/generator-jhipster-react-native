7fbe93c78778aea5bb7f8c3ccbd144f03f05ad08
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2019-04-05 20:33:18 -0400

    upgrade to ignite v3

--- commands/upgrade.js
+++
@@ -1,2 +0,0 @@
-// @cliDescription  Upgrades an existing IgniteJHipster project to the latest boilerplate code
-module.exports = require('../src/upgrade')
--- src/entity/index.js
+++ src/entity/index.js
@@ -2,73 +2,75 @@ const fs = require('fs-extra')
 const Insight = require('../lib/insight')
 const generateFiles = require('./files')
 const prompts = require('./prompts')
+module.exports = {
+  description: 'Generates an entity component, redux, saga, api, listings, styles, and optional tests.',
+  run: async function (context) {
+    // grab some features
+    const { ignite, parameters, print, prompt, strings } = context
+    const { pascalCase, isBlank } = strings
 
-module.exports = async function (context) {
-  // grab some features
-  const { ignite, parameters, print, prompt, strings } = context
-  const { pascalCase, isBlank } = strings
+    // flags
+    const jhDirectoryFlag = parameters.options['jh-dir'] || ''
 
-  // flags
-  const jhDirectoryFlag = parameters.options['jh-dir'] || ''
-
-  // validation
-  if (isBlank(parameters.first)) {
-    print.info(`${context.runtime.brand} generate entity <name>\n`)
-    print.info('A name is required.')
-    return
-  }
-
-  // load the ignite config and set the default jhipster directory
-  this.igniteConfig = ignite.loadIgniteConfig()
-  this.name = pascalCase(parameters.first)
-  prompts.entityPrompts[0].default = this.igniteConfig.jhipsterDirectory
-
-  // read some configuration
-  const entityFileName = `${this.name}.json`
-  const localEntityFilePath = `.jhipster/${entityFileName}`
-
-  let fullEntityFilePath
-  let jhipsterDirectory
-
-  // if the file exists, skip loading it
-  if (fs.existsSync(localEntityFilePath)) {
-    print.success(`Found the ${this.name} entity config locally in .jhipster`)
-  } else if (jhDirectoryFlag) {
-    if (!fs.existsSync(`${jhDirectoryFlag}/${localEntityFilePath}`)) {
-      print.error(`No entity configuration file found at ${jhDirectoryFlag}/${localEntityFilePath}`)
+    // validation
+    if (isBlank(parameters.first)) {
+      print.info(`${context.runtime.brand} generate entity <name>\n`)
+      print.info('A name is required.')
       return
     }
-    print.success(`Found the entity config at ${jhDirectoryFlag}/${localEntityFilePath}`)
-    jhipsterDirectory = jhDirectoryFlag
-    fullEntityFilePath = `${jhDirectoryFlag}/${localEntityFilePath}`
-  } else {
-    // prompt the user until an entity configuration file is found
-    while (true) {
-      let entityAnswers = await prompt.ask(prompts.entityPrompts)
-      // strip the trailing slash from the directory
-      jhipsterDirectory = `${entityAnswers.filePath}`.replace(/\/$/, ``)
-      fullEntityFilePath = `${jhipsterDirectory}/.jhipster/${entityFileName}`
-      print.info(`Looking for ${fullEntityFilePath}`)
-      if (fs.existsSync(fullEntityFilePath)) {
-        print.success(`Found entity file at ${fullEntityFilePath}`)
-        break
-      } else {
-        print.error(`Could not find entity file, please try again.`)
+
+    // load the ignite config and set the default jhipster directory
+    this.igniteConfig = ignite.loadIgniteConfig()
+    this.name = pascalCase(parameters.first)
+    prompts.entityPrompts[0].default = this.igniteConfig.jhipsterDirectory
+
+    // read some configuration
+    const entityFileName = `${this.name}.json`
+    const localEntityFilePath = `.jhipster/${entityFileName}`
+
+    let fullEntityFilePath
+    let jhipsterDirectory
+
+    // if the file exists, skip loading it
+    if (fs.existsSync(localEntityFilePath)) {
+      print.success(`Found the ${this.name} entity config locally in .jhipster`)
+    } else if (jhDirectoryFlag) {
+      if (!fs.existsSync(`${jhDirectoryFlag}/${localEntityFilePath}`)) {
+        print.error(`No entity configuration file found at ${jhDirectoryFlag}/${localEntityFilePath}`)
+        return
+      }
+      print.success(`Found the entity config at ${jhDirectoryFlag}/${localEntityFilePath}`)
+      jhipsterDirectory = jhDirectoryFlag
+      fullEntityFilePath = `${jhDirectoryFlag}/${localEntityFilePath}`
+    } else {
+      // prompt the user until an entity configuration file is found
+      while (true) {
+        let entityAnswers = await prompt.ask(prompts.entityPrompts)
+        // strip the trailing slash from the directory
+        jhipsterDirectory = `${entityAnswers.filePath}`.replace(/\/$/, ``)
+        fullEntityFilePath = `${jhipsterDirectory}/.jhipster/${entityFileName}`
+        print.info(`Looking for ${fullEntityFilePath}`)
+        if (fs.existsSync(fullEntityFilePath)) {
+          print.success(`Found entity file at ${fullEntityFilePath}`)
+          break
+        } else {
+          print.error(`Could not find entity file, please try again.`)
+        }
       }
     }
-  }
 
-  if (fullEntityFilePath) {
-    await fs.copy(fullEntityFilePath, localEntityFilePath)
-    print.success(`Entity config saved to your app's .jhipster folder.`)
-  }
-  if (jhipsterDirectory) {
-    // save the jhipster app directory to the ignite config as the new jhipsterDirectory default
-    this.igniteConfig.jhipsterDirectory = jhipsterDirectory
-    await fs.writeJson('ignite/ignite.json', this.igniteConfig, { spaces: '\t' })
-  }
-  await generateFiles(this, context)
+    if (fullEntityFilePath) {
+      await fs.copy(fullEntityFilePath, localEntityFilePath)
+      print.success(`Entity config saved to your app's .jhipster folder.`)
+    }
+    if (jhipsterDirectory) {
+      // save the jhipster app directory to the ignite config as the new jhipsterDirectory default
+      this.igniteConfig.jhipsterDirectory = jhipsterDirectory
+      await fs.writeJson('ignite/ignite.json', this.igniteConfig, { spaces: '\t' })
+    }
+    await generateFiles(this, context)
 
-  Insight.trackGenerator(context, 'entity')
-  print.success(`Entity successfully generated!`)
+    Insight.trackGenerator(context, 'entity')
+    print.success(`Entity successfully generated!`)
+  }
 }
