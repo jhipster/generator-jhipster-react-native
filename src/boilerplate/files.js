df7122755acb1fbce6e2e8129a497b5617670f6b
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2019-04-07 20:13:26 -0700

    updates related to oauth changes in main generator

--- src/boilerplate/files.js
+++ src/boilerplate/files.js
@@ -167,8 +167,8 @@ module.exports = async function (context, props, jhipsterConfig) {
       if (fs.existsSync(`${jhipsterPathPrefix}${props.jhipsterDirectory}/${keycloakConfigFile}`)) {
         await ignite.patchInFile(`${jhipsterPathPrefix}${props.jhipsterDirectory}/${keycloakConfigFile}`,
           {
-            replace: `"redirectUris": ["http://localhost:8080/*",`,
-            insert: `"redirectUris": ["http://localhost:8080/*", "${props.name.toLowerCase()}://*",`
+            replace: `"http://localhost:8080/*",`,
+            insert: `"http://localhost:8080/*", "${props.name.toLowerCase()}://*",`
           }
         )
       }
