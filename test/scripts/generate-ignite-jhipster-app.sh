2402999c5dbde70f4a1095b87b1161d95c05d9ad
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2019-07-27 18:09:13 -0700

    add websocket pipeline to azure pipelines
    splits jhipster backend generation and packaging so ignite-jhipster can add changes for websockets and oauth2

--- test/scripts/generate-ignite-jhipster-app.sh
+++ test/scripts/generate-ignite-jhipster-app.sh
@@ -3,6 +3,14 @@
 # switch to the directory containing the jhipster app folder
 cd ../
 # generate the app using the same JDL as the backend
-ignite new ${JHI_IGNITE_APP_NAME} -b ${BUILD_REPOSITORY_LOCALPATH} --jdl=./${SYSTEM_JOBNAME}.jdl --detox=true --disable-insight --skip-git
+
+if [ "$JHI_AUTH_TYPE" = "oauth2" ] || [ "$JHI_WEBSOCKETS" = true ]; then
+#  use --jh-dir for oauth2 and websocket apps since they change backend files
+  ignite new ${JHI_IGNITE_APP_NAME} -b ${BUILD_REPOSITORY_LOCALPATH} --jh-dir=./${SYSTEM_JOBNAME}-backend --detox=true --disable-insight --skip-git
+else
+#  use --jdl for other apps to test both generation methods
+  ignite new ${JHI_IGNITE_APP_NAME} -b ${BUILD_REPOSITORY_LOCALPATH} --jdl=./${SYSTEM_JOBNAME}.jdl --detox=true --disable-insight --skip-git
+fi
+
 # list files
 ls -al ${JHI_IGNITE_APP_NAME}
--- test/scripts/package-jhipster-backend.sh
+++ test/scripts/package-jhipster-backend.sh
@@ -2,17 +2,12 @@
 
 set -e
 
-# switch to a directory to generate the backend in
-mkdir ../${SYSTEM_JOBNAME}-backend
+# switch to the directory the backend was generated in
 cd ../${SYSTEM_JOBNAME}-backend
 ls -al
-# generate the app
-jhipster import-jdl ../${SYSTEM_JOBNAME}.jdl --force --skip-checks --skip-git --skip-commit-hook --from-cli --force-insight --skip-install
-# list files
-ls -al
-# display jhipster info
-jhipster info
 
-# switch to a directory to generate the backend in
+# show files modified by ignite-jhipster
+git status
+
 # package the app into a WAR
 ./mvnw clean package -Pprod -DskipTests
