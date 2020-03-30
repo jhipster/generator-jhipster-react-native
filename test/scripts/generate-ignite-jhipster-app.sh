0743f4781584783b92e12c170249879917bf2e83
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2020-03-30 18:58:25 -0400

    skip pod install for android e2e

--- test/scripts/generate-ignite-jhipster-app.sh
+++ test/scripts/generate-ignite-jhipster-app.sh
@@ -6,13 +6,18 @@ set -x;
 cd ../
 rm -rf ${JHI_IGNITE_APP_NAME}
 # generate the app using the same JDL as the backend
+SKIP_POD_INSTALL=""
+if [ "$PLATFORM" = "android" ]; then
+  SKIP_POD_INSTALL=" --skip-pod-install"
+fi
+
 
 if [ "$JHI_AUTH_TYPE" = "oauth2" ] || [ "$JHI_WEBSOCKETS" = true ]; then
 #  use --jh-dir for oauth2 and websocket apps since they change backend files
-  ignite new ${JHI_IGNITE_APP_NAME} -b ${GITHUB_WORKSPACE} --jh-dir=./${JHI_IGNITE_APP_NAME}-backend --detox=true --disable-insight --skip-git
+  ignite new ${JHI_IGNITE_APP_NAME} -b ${GITHUB_WORKSPACE} --jh-dir=./${JHI_IGNITE_APP_NAME}-backend --detox=true --disable-insight --skip-git ${SKIP_POD_INSTALL}
 else
 #  use --jdl for other apps to test both generation methods
-  ignite new ${JHI_IGNITE_APP_NAME} -b ${GITHUB_WORKSPACE} --jdl=./${JHI_IGNITE_APP_NAME}.jdl --detox=true --disable-insight --skip-git
+  ignite new ${JHI_IGNITE_APP_NAME} -b ${GITHUB_WORKSPACE} --jdl=./${JHI_IGNITE_APP_NAME}.jdl --detox=true --disable-insight --skip-git ${SKIP_POD_INSTALL}
 fi
 
 # list files
