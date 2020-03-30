302bae72fb2a284f1cf3ae177388c75f1d9a70e9
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2020-03-30 19:46:13 -0400

    update badges, remove react-native package install from CI

--- test/scripts/install-node-dependencies.sh
+++ test/scripts/install-node-dependencies.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-npm install -g generator-jhipster react-native ignite-cli --ignore-scripts spawn-sync
+npm install -g generator-jhipster ignite-cli --ignore-scripts spawn-sync
