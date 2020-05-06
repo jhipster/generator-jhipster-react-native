bbd8b9ca2f8b0c2ec441afb3d005874d731a62ba
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2020-05-06 11:15:02 -0400

    increase android emulator memory

--- test/scripts/start-android-emulator.sh
+++ test/scripts/start-android-emulator.sh
@@ -7,7 +7,7 @@ $ANDROID_HOME/platform-tools/adb devices
 
 echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd -n Nexus_6_API_29 -k 'system-images;android-29;default;x86_64' --force  > /dev/null
 
-nohup $ANDROID_HOME/emulator/emulator -avd Nexus_6_API_29 -no-snapshot -memory 2048 > /dev/null 2>&1 & $ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82'
+nohup $ANDROID_HOME/emulator/emulator -avd Nexus_6_API_29 -no-snapshot -memory 3072 > /dev/null 2>&1 & $ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82'
 
 # disable animations
 $ANDROID_HOME/platform-tools/adb shell settings put global window_animation_scale 0.0
