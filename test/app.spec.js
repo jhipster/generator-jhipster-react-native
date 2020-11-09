f2a994fc18e9313a5250cad90884d9157fb5a504
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2020-11-09 18:31:04 -0500

    add initial test with expected files

--- test/app.spec.js
+++ test/app.spec.js
@@ -2,11 +2,12 @@ const path = require('path');
 const fse = require('fs-extra');
 const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
+const expectedFiles = require('./expected-files');
 
 // todo maybe deal with https://github.com/jhipster/generator-jhipster/pull/12903/files#diff-6b3a4e19c79c8b302650b8b7466ce6e01e78d0e2f3105d87e9e55b9d6f4cb32d
 
 describe('Subgenerator app of react-native JHipster blueprint', () => {
-    describe('Sample test', () => {
+    describe('Default App Test', () => {
         before(done => {
             helpers
                 .run('generator-jhipster/generators/app')
@@ -18,6 +19,7 @@ describe('Subgenerator app of react-native JHipster blueprint', () => {
                     skipInstall: true,
                     blueprint: 'react-native',
                     skipChecks: true,
+                    debug: true,
                 })
                 .withGenerators([
                     [
@@ -26,27 +28,20 @@ describe('Subgenerator app of react-native JHipster blueprint', () => {
                         path.join(__dirname, '../generators/app/index.js'),
                     ],
                 ])
-                .withPrompts({
-                    baseName: 'sampleMysql',
-                    packageName: 'com.mycompany.myapp',
-                    applicationType: 'monolith',
-                    databaseType: 'sql',
-                    devDatabaseType: 'h2Disk',
-                    prodDatabaseType: 'mysql',
-                    cacheProvider: 'ehcache',
-                    authenticationType: 'session',
-                    enableTranslation: true,
-                    nativeLanguage: 'en',
-                    languages: ['fr', 'de'],
-                    buildTool: 'maven',
-                    rememberMeKey: '2bb60a80889aa6e6767e9ccd8714982681152aa5',
-                })
+                // .withPrompts({
+                //     detox: false,
+                //     appName: 'RnApp',
+                //     directoryPath: '../backend',
+                // })
                 .on('end', done);
         });
 
-        it('it works', () => {
+        it('it generates the expected files', () => {
             // Adds your tests here
-            assert.textEqual('Write your own tests!', 'Write your own tests!');
+            assert.file(expectedFiles.defaultApp);
+            assert.noFile(expectedFiles.detox);
+            assert.noFile(expectedFiles.oauth);
+            assert.noFile(expectedFiles.websockets);
         });
     });
 });
