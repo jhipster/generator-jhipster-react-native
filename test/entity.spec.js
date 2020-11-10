68b38b0f7e4f8da5e64216aab98e93b79f216191
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2020-11-10 17:49:37 -0500

    add test for existing entity JSON file

--- test/entity.spec.js
+++ test/entity.spec.js
@@ -5,6 +5,36 @@ const helpers = require('yeoman-test');
 const expectedFiles = require('./expected-files');
 
 describe('Subgenerator entity', () => {
+    describe('Generates entity from existing JSON config file', () => {
+        before(done => {
+            helpers
+                .run('generator-jhipster/generators/entity')
+                .inTmpDir(dir => {
+                    fse.copySync(path.join(__dirname, '../test/templates/backend'), `${dir}/`);
+                })
+                .withOptions({
+                    'from-cli': true,
+                    skipInstall: true,
+                    blueprint: 'react-native',
+                    skipChecks: true,
+                })
+                .withGenerators([
+                    [
+                        require('../generators/entity'), // eslint-disable-line global-require
+                        'jhipster-react-native:entity',
+                        path.join(__dirname, '../generators/entity/index.js'),
+                    ],
+                ])
+                .withArguments(['foo'])
+                .on('end', done);
+        });
+
+        it('it generates the expected files', () => {
+            assert.file(expectedFiles.entity);
+            assert.noFile(expectedFiles.entityDetox);
+            assert.noFile(expectedFiles.entitySearchEngine);
+        });
+    });
     describe('Generates entity from prompt of backend path', () => {
         before(done => {
             helpers
