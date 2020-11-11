e047ef76c5260bab3324656ea9adf361149a1dea
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2020-11-11 16:14:36 -0500

    use master generator-jhipster

--- test/scripts/install-node-dependencies.sh
+++ test/scripts/install-node-dependencies.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+git clone https://github.com/jhipster/generator-jhipster.git ../generator-jhipster
+cd ../generator-jhipster && git checkout 819d07f1ca349ae6f8710f2a3387030a29964982 && npm link
+
 # set yarn network-timeout to 10 minutes
 yarn config set network-timeout 600000
