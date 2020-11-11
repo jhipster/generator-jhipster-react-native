6b435e95fb9c8e7029411da1b5e1688ea5649c0d
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2020-11-11 16:38:50 -0500

    pass defaults flag to import-jdl command

--- test/scripts/generate-react-native-app.sh
+++ test/scripts/generate-react-native-app.sh
@@ -11,11 +11,13 @@ rm -rf ${JHI_REACT_NATIVE_APP_NAME} && mkdir ${JHI_REACT_NATIVE_APP_NAME} && cd 
 if [ "$JHI_AUTH_TYPE" = "oauth2" ] || [ "$JHI_WEBSOCKETS" = true ]; then
 # generate the app using the backend directory
 #  use JHipster directory for oauth2 and websocket apps since they change backend files
-  jhipster --blueprints react-native --defaults --no-insight --skip-git --skip-install
+  jhipster --blueprints react-native \
+  --defaults --no-insight --skip-git --skip-install
 else
 # generate the app using the same JDL as the backend
 #  use JDL for other apps to test both generation methods
-  jhipster --blueprints react-native import-jdl ../${JHI_REACT_NATIVE_APP_NAME}.jdl --no-insight --skip-install
+  jhipster --blueprints react-native import-jdl ../${JHI_REACT_NATIVE_APP_NAME}.jdl \
+  --defaults --no-insight --skip-git --skip-install
 fi
 
 # list files
