7b7f7e53d234f299686db2337a14653103e6778d
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2020-12-16 18:08:48 -0500

    remove custom _setUpvariables in favor of jhipster autoconfiguration

--- generators/entity/index.js
+++ generators/entity/index.js
@@ -1,13 +1,19 @@
-const pluralize = require('pluralize');
 const chalk = require('chalk');
 const EntityGenerator = require('generator-jhipster/generators/entity');
 const { askForBackendJson } = require('./prompts');
 const { writeFiles } = require('./files');
-const { patchInFile, patchNavigationForEntity, patchEntityApi, getEntityFormField, getRelationshipFormField } = require('../../lib');
+const {
+    patchInFile,
+    patchNavigationForEntity,
+    patchEntityApi,
+    getEntityFormField,
+    getRelationshipFormField,
+    getFieldValidateType,
+} = require('../../lib');
 
 module.exports = class extends EntityGenerator {
     constructor(args, opts) {
-        super(args, { fromBlueprint: true, ...opts }); // fromBlueprint variable is important
+        super(args, { fromBlueprint: true, angularSuffix: '', ...opts }); // fromBlueprint variable is important
 
         const jhContext = (this.jhipsterContext = this.options.jhipsterContext);
 
@@ -22,7 +28,7 @@ module.exports = class extends EntityGenerator {
     }
 
     get initializing() {
-        return {};
+        return super._initializing();
     }
 
     get prompting() {
@@ -31,148 +37,33 @@ module.exports = class extends EntityGenerator {
         };
     }
 
-    _setUpVariables() {
-        this.searchEngine = this.config.get('searchEngine');
-
-        const reactNativeConfig = this.config.get('reactNative');
-        if (reactNativeConfig) {
-            this.detox = reactNativeConfig.detox;
-        } else {
-            this.info('Defaulting to false for Detox');
-            this.detox = false;
-        }
-
-        this.name = this.options.name;
-        this.pluralName = pluralize(this.name);
-        this.camelCaseName = this._.camelCase(this.name);
-        this.camelCaseNamePlural = pluralize(this.camelCaseName);
-        this.kebabCaseName = this._.kebabCase(this.name);
-        this.kebabCaseNamePlural = this._.kebabCase(this.pluralName);
-        this.snakeCaseName = this._.snakeCase(this.name);
-        this.context.entityNameCapitalized = this._.upperFirst(this.name);
-
-        // todo remove - used in relationships
-        this.startCase = this._.startCase;
-        this.camelCase = this._.camelCase;
-        this.kebabCase = this._.kebabCase;
-        this.upperFirst = this._.upperFirst;
-        this.pascalCase = str => this._.upperFirst(this._.camelCase(str));
-
-        this.getEntityFormField = getEntityFormField.bind(this);
-        this.getRelationshipFormField = getRelationshipFormField.bind(this);
-
-        // load entity JSON from config file
-        this.entityInfo = this.context.entityJSON;
-        this.entityContainsImageBlob = false;
-        this.entityContainsTextBlob = false;
-        this.entityContainsDate = false;
-        this.entityContainsLocalDate = false;
-
-        this.entityInfo.fields.forEach(field => {
-            // enums
-            field.fieldIsEnum = ![
-                'String',
-                'Integer',
-                'Long',
-                'Float',
-                'Double',
-                'BigDecimal',
-                'LocalDate',
-                'Instant',
-                'ZonedDateTime',
-                'Duration',
-                'UUID',
-                'Boolean',
-                'byte[]',
-                'ByteBuffer',
-            ].includes(field.fieldType);
-
-            // blobs
-            if (field.fieldType === 'byte[]' && field.fieldTypeBlobContent === 'image') {
-                this.entityContainsImageBlob = true;
-            }
-            if (field.fieldType === 'byte[]' && field.fieldTypeBlobContent === 'text') {
-                this.entityContainsTextBlob = true;
-            }
-
-            // dates
-            if (field.fieldType === 'LocalDate') {
-                this.entityContainsLocalDate = true;
-            }
-            if (field.fieldType === 'LocalDate' || field.fieldType === 'ZonedDateTime' || field.fieldType === 'Instant') {
-                this.entityContainsDate = true;
-            }
-
-            // validation
-            field.fieldValidate = Array.isArray(field.fieldValidateRules) && field.fieldValidateRules.length >= 1;
-            switch (field.fieldType) {
-                case 'LocalDate':
-                case 'ZonedDateTime':
-                case 'Instant':
-                    field.fieldValidateType = 'date';
-                    break;
-                case 'Integer':
-                case 'Long':
-                case 'Float':
-                case 'Double':
-                case 'BigDecimal':
-                    field.fieldValidateType = 'number';
-                    break;
-                case 'byte[]':
-                case 'String':
-                case 'Duration':
-                default:
-                    field.fieldValidateType = 'string';
-            }
-
-            field.nullable = !(field.fieldValidate === true && field.fieldValidateRules.includes('required'));
-            field.unique = field.fieldValidate === true && field.fieldValidateRules.includes('unique');
-        });
-        // these lists are to prevent double imports when there are multiple relations between the same entity
-        const alreadyIncludedEntities = [];
-        const uniqueEntityRelationships = [];
-        this.entityInfo.relationships.forEach(relation => {
-            if (relation.relationshipType === 'many-to-one') {
-                relation.ownerSide = true;
-            }
-            relation.otherEntityNamePlural = pluralize.plural(relation.otherEntityName);
-            relation.relationshipNamePlural = pluralize.plural(relation.relationshipName);
-            if (!alreadyIncludedEntities.includes(relation.otherEntityName)) {
-                alreadyIncludedEntities.push(relation.otherEntityName);
-                uniqueEntityRelationships.push(relation);
-            }
-        });
-
-        this.entityInfo.uniqueOwnerSideRelationships = uniqueEntityRelationships.filter(relation => relation.ownerSide);
-        this.entityInfo.ownerSideRelationships = this.entityInfo.relationships.filter(relation => relation.ownerSide);
-
-        // todo this
-        this.microservicePath = '';
-        // if a microservice name is available, set the path prefix in the API paths
-        if (Object.prototype.hasOwnProperty.call(this.entityInfo, 'microserviceName')) {
-            this.microservicePath = `services/${this.entityInfo.microserviceName}/`;
-        }
-
-        // todo
-        this.hasValidationRule = this.entityInfo.fields.some(
-            field =>
-                field.fieldValidate &&
-                (field.fieldValidateRules.includes('required') ||
-                    field.fieldValidateRules.includes('minlength') ||
-                    field.fieldValidateRules.includes('maxlength') ||
-                    field.fieldValidateRules.includes('min') ||
-                    field.fieldValidateRules.includes('max') ||
-                    field.fieldValidateRules.includes('pattern'))
-        );
-
-        this.hasRequiredRelationship = this.entityInfo.relationships.some(relationship => relationship.relationshipRequired);
-        this.isValidatorsRequired = this.hasValidationRule || this.hasRequiredRelationship;
+    get configuring() {
+        return super._configuring();
     }
 
-    get configuring() {
+    get loading() {
+        return super._loading();
+    }
+
+    get preparing() {
         return {
-            setUpVariables: this._setUpVariables,
+            ...super._preparing(),
+            addHelpers() {
+                // add custom methods to the context
+                this.context.getEntityFormField = getEntityFormField.bind(this);
+                this.context.getRelationshipFormField = getRelationshipFormField.bind(this);
+                this.context.getFieldValidateType = getFieldValidateType.bind(this);
+
+                // log the context for debugging purposes
+                if ((this.configOptions && this.configOptions.isDebugEnabled) || (this.options && this.options.debug)) {
+                    console.log(this.context);
+                }
+            },
         };
+    }
+
+    get preparingRelationships() {
+        return super._preparingRelationships();
     }
 
     get default() {
