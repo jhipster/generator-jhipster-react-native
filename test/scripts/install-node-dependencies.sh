03909374fc401b3dbfcb330b0256ab9e78f1a6e4
Jon Ruddell <jonathan.ruddell@gmail.com>
2020-12-23 17:57:26 -0500

    pin ignite v3

--- test/scripts/install-node-dependencies.sh
+++ test/scripts/install-node-dependencies.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-npm install -g generator-jhipster ignite-cli --ignore-scripts spawn-sync
+npm install -g generator-jhipster ignite-cli@3 --ignore-scripts spawn-sync
 
 # set yarn network-timeout to 10 minutes
 yarn config set network-timeout 600000
