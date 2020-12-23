12d5332aa15634bcf92876ef51fbe62e088a31ec
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2020-12-23 17:22:19 -0500

    lint and prettier

--- generators/entities/index.js
+++ generators/entities/index.js
@@ -30,7 +30,6 @@ module.exports = class extends EntitiesGenerator {
             console.log('No jhContext found after initializing blueprint (entities generator)');
             this.error(`This is a JHipster blueprint and should be used only like ${chalk.yellow('jhipster --blueprints react-native')}`);
         }
-
     }
 
     get initializing() {
