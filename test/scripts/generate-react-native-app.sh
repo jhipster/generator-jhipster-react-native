ebed063b589195402a88b1ab8d453a07b5d3aab8
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2021-01-18 18:26:52 -0500

    cleanup test scripts, always use JDL instead of backend since no changes are needed

--- test/scripts/generate-react-native-app.sh
+++ test/scripts/generate-react-native-app.sh
@@ -1,26 +1,14 @@
 #!/usr/bin/env bash
 set -ex
 
-# switch to the directory containing the jhipster app folder
-cd ../
+# switch to a directory to generate the react-native client
+rm -rf ../${JHI_REACT_NATIVE_APP_NAME} && mkdir ../${JHI_REACT_NATIVE_APP_NAME} && cd ../${JHI_REACT_NATIVE_APP_NAME}
 
-# clear the folder (todo: may not be needed?)
-rm -rf ${JHI_REACT_NATIVE_APP_NAME} && mkdir ${JHI_REACT_NATIVE_APP_NAME} && cd ${JHI_REACT_NATIVE_APP_NAME}
-
-# for the GITHUB_WORKFLOW = app, we always use JDL
-if [ "$GITHUB_WORKFLOW" = "iOS-E2E" ] && [ "$JHI_WEBSOCKETS" = true ]; then
-    echo "Using Backend Path for Generation"
-# generate the app using the backend directory
-#  use JHipster directory for oauth2 and websocket apps since they change backend files
-  jhipster --blueprints react-native \
-  --defaults --no-insight --skip-git --with-entities
-else
-    echo "Using JDL for Generation"
+echo "Using JDL for Generation"
 # generate the app using the same JDL as the backend
 #  use JDL for other apps to test both generation methods
-  jhipster --blueprints react-native import-jdl ../${JHI_REACT_NATIVE_APP_NAME}.jdl \
+jhipster --blueprints react-native import-jdl ../${JHI_REACT_NATIVE_APP_NAME}.jdl \
   --defaults --no-insight --skip-git
-fi
 
 # list files
 ls -al
--- test/scripts/install-detox.sh
+++ test/scripts/install-detox.sh
@@ -5,5 +5,3 @@ if [ "$PLATFORM" = "ios" ]; then
   brew tap wix/brew
   brew install applesimutils
 fi
-
-npm install -g detox-cli
