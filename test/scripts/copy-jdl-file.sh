95d38f2784e021b985283aad340d7c3d2eb87b09
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2021-01-19 14:27:48 -0500

    add more tests to app and e2e, fixes for those tests
    fix chat screen e2e test
    add retries to e2e tests in place of using retry action
    remove waitForSplashScreen method in e2e utils
    update copy-jdl-file to customize app/entity JDL
    allow specifying JDL file for e2e tests to use

--- test/scripts/copy-jdl-file.sh
+++ test/scripts/copy-jdl-file.sh
@@ -1,17 +1,35 @@
 #!/usr/bin/env bash
-set -ex
+set -e
 
-if [ "$JHI_DTO" = true ] ; then
-  DTO_SUFFIX="-dto"
-fi
-if [ "$JHI_WEBSOCKETS" = true ] ; then
-  WEBSOCKET_SUFFIX="-websockets"
-fi
+# use the specified app JDL file, or default to app.jdl
+APP_JDL=${JHI_APP_JDL:-"app.jdl"}
 
-APP_JDL="app-${JHI_AUTH_TYPE}${WEBSOCKET_SUFFIX}.jdl"
-ENTITY_JDL="entities${DTO_SUFFIX}.jdl"
+# use the specified entity JDL file, or default to entities.jdl
+ENTITY_JDL=${JHI_ENTITY_JDL:-"entities.jdl"}
 
 echo "Using JDL files: ${APP_JDL} ${ENTITY_JDL}"
+echo "Using Options: ${APP_JDL} ${ENTITY_JDL}"
 
 # combine the app and entity JDL file
 cat ${GITHUB_WORKSPACE}/test/jdl/${APP_JDL} ${GITHUB_WORKSPACE}/test/jdl/${ENTITY_JDL} >> ../${JHI_REACT_NATIVE_APP_NAME}.jdl
+
+echo "      Auth Type: ${JHI_AUTH_TYPE}"
+sed -i.back "s~authenticationType jwt~authenticationType ${JHI_AUTH_TYPE}~g" ../${JHI_REACT_NATIVE_APP_NAME}.jdl
+
+echo "      Websockets: ${JHI_WEBSOCKETS}"
+if [ "$JHI_WEBSOCKETS" = true ] ; then
+    sed -i.back "s~websocket false~websocket spring-websocket~g" ../${JHI_REACT_NATIVE_APP_NAME}.jdl
+fi
+
+echo "      DTO:        ${JHI_DTO}"
+if [ "$JHI_DTO" = true ] ; then
+    echo "dto * with mapstruct" >> ../${JHI_REACT_NATIVE_APP_NAME}.jdl
+fi
+
+echo ""
+echo ""
+echo "Using JDL:"
+echo ""
+echo ""
+
+cat ../${JHI_REACT_NATIVE_APP_NAME}.jdl
