7ee8096c6ce4b70bd767bfadb1ae2607ca0bcae6
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2021-01-26 16:20:05 -0500

    add brotli support to heroku

--- generators/heroku/index.js
+++ generators/heroku/index.js
@@ -101,8 +101,8 @@ module.exports = class extends HerokuGenerator {
       addHerokuDependencies() {
         const packageJsonSource = JSON.parse(fs.readFileSync('package.json', 'utf-8'));
         packageJsonSource.dependencies['http-server'] = '0.12.3';
-        packageJsonSource.scripts['heroku-postbuild'] = 'npm run build:web';
-        packageJsonSource.scripts['heroku-prebuild'] = 'npm install -g expo-cli http-server';
+        packageJsonSource.scripts['heroku-prebuild'] = 'npm install -g expo-cli http-server gzipper';
+        packageJsonSource.scripts['heroku-postbuild'] = 'npm run build:web && gzipper compress ./web-build --brotli';
         this.fs.writeJSON('package.json', packageJsonSource);
       },
     };
