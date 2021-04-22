b27373b042bfced9116e34b231da084aa692ce97
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2021-04-22 08:32:53 -0400

    update expo to sdk 41
    update expo dependencies
    pin expo SDK version, let dependabot notify about updates
    replace async-storage with new library

--- test/scripts/install-node-dependencies.sh
+++ test/scripts/install-node-dependencies.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 set -exo pipefail
 
+# install npm v7
+npm i -g npm@7
+
+# install expo, JHipster, and Detox CLIs
 npm i -g expo-cli generator-jhipster@$JHIPSTER_VERSION detox-cli
