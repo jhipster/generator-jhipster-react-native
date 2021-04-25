b23308b4c88e672dd278d7823773f7c8be4aa3b8
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2021-04-25 15:28:22 -0400

    install heroku with npm for sample app

--- test/scripts/install-heroku-cli.sh
+++ test/scripts/install-heroku-cli.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 set -exo pipefail
 
-if [[ $OS == *"ubuntu"* ]]; then
-    sudo snap install --classic heroku
-else
-    brew tap heroku/brew
-    brew install heroku
-fi
+npm install -g heroku
 
 heroku whoami
