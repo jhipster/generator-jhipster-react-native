e6d804539adbb52d20cd91d08a62669bf641826d
Jonathan Ruddell <jonathan.ruddell@gmail.com>
2021-11-22 21:48:02 -0500

    update jhipster version

--- test/scripts/generate-react-native-app.sh
+++ test/scripts/generate-react-native-app.sh
@@ -6,14 +6,15 @@ rm -rf ../${JHI_REACT_NATIVE_APP_NAME} && mkdir ../${JHI_REACT_NATIVE_APP_NAME} 
 
 echo "Using JDL for Generation"
 
+# todo: debug fix for why --blueprints flag no longer works
 # generate the app using the same JDL as the backend
-if [ "$JHIPSTER_VERSION" = "6" ]; then
+#if [ "$JHIPSTER_VERSION" = "6" ]; then
   rnhipster jdl ../${JHI_REACT_NATIVE_APP_NAME}.jdl \
   --defaults --no-insight --skip-git
-else
-  jhipster --blueprints react-native jdl ../${JHI_REACT_NATIVE_APP_NAME}.jdl \
-  --defaults --no-insight --skip-git
-fi
+#else
+#  jhipster --blueprints react-native jdl ../${JHI_REACT_NATIVE_APP_NAME}.jdl \
+#  --defaults --no-insight --skip-git
+#fi
 
 # list files
 ls -al
