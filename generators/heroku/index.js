4eaebb5fa97b0ebdab0b8472f61f74bac5d0b139
Marcelo Boveto Shima <marceloshima@gmail.com>
2023-08-04 17:25:31 -0300

    update dependencies.

--- generators/heroku/index.js
+++ generators/heroku/index.js
@@ -106,7 +106,7 @@ module.exports = class extends HerokuGenerator {
         const packageJsonSource = JSON.parse(fs.readFileSync('package.json', 'utf-8'));
         packageJsonSource.dependencies['http-server'] = '14.1.1';
         packageJsonSource.scripts['heroku-prebuild'] = 'npm install -g sharp-cli http-server gzipper generator-jhipster-react-native';
-        packageJsonSource.scripts['heroku-postbuild'] = 'npm run build:web && gzipper compress ./web-build --brotli';
+        packageJsonSource.scripts['heroku-postbuild'] = 'npm run build:web && gzipper compress ./dist --brotli';
         this.fs.writeJSON('package.json', packageJsonSource);
       },
     };
