611d8324fae8e2e5a70e084b8d310dfc01eb3c32
Hideyuki Kagami <hide212131@gmail.com>
2024-02-04 21:05:09 +0900

    adjust detox default enabled

--- generators/react-native/generator.mjs
+++ generators/react-native/generator.mjs
@@ -52,7 +52,8 @@ export default class extends BaseApplicationGenerator {
       this.jhipsterConfig.authenticationType = this.options.authenticationType;
     }
     if (this.options.defaults || this.options.force) {
-      this.reactNativeStorage.defaults({ appDir: DEFAULT_BACKEND_PATH, reactNativeDir: null, detox: DEFAULT_ENABLE_DETOX });
+      // using defaults options, detox is enebled by default
+      this.reactNativeStorage.defaults({ appDir: DEFAULT_BACKEND_PATH, reactNativeDir: null, detox: true });
     }
   }
 
