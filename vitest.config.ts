2d07acbbe4b44387ffb344c7b5c49033ba1388f7
Dwarakaprasad Thakku Damodaran <dwarka.damodaran@gmail.com>
2024-05-08 21:10:38 +0000

    increate vitest timeout

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, defaultExclude } from 'vitest/config';
 export default defineConfig({
   test: {
     pool: 'forks',
-    hookTimeout: 20000,
+    hookTimeout: 200000,
     exclude: [...defaultExclude.filter(val => val !== '**/cypress/**'), '**/templates/**', '**/resources/**'],
   },
 });
