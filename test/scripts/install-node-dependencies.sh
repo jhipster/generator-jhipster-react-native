b931ef5972b8858ce3d5c87759fd5e4f06a415d5
Marcelo Shima <marceloshima@gmail.com>
2024-05-09 17:52:36 -0300

    cleanup install-node-dependencies.sh

--- test/scripts/install-node-dependencies.sh
+++ test/scripts/install-node-dependencies.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -exo pipefail
 
-# install EAS, Sharp (for images), JHipster, and Detox CLIs
-npm i -g eas-cli sharp-cli generator-jhipster@$JHIPSTER_VERSION detox-cli
+# install JHipster
+npm i -g generator-jhipster@$JHIPSTER_VERSION
