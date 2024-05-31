f321b338bb14e2c66a70b1304e4107635f0e8e92
mshima <1417183+mshima@users.noreply.github.com>
2024-05-31 23:41:56 +0000

    Update generator-jhipster to 8.5.0

--- cli/cli.cjs
+++ cli/cli.cjs
@@ -17,7 +17,7 @@ const blueprint = packageFolderName.startsWith('jhipster-') ? `generator-${packa
   runJHipster({
     executableName,
     executableVersion: version,
-    defaultCommand: 'react-native',
+    defaultCommand: 'app',
     devBlueprintPath,
     blueprints: {
       [blueprint]: version,
