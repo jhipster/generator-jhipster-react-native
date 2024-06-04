02e1035632b0ead5ca2de8cb1e093c6371ab14a6
Marcelo Boveto Shima <marceloshima@gmail.com>
2024-06-04 11:05:49 -0300

    install adjusts

--- generators/react-native/generator.mjs
+++ generators/react-native/generator.mjs
@@ -337,9 +337,10 @@ export default class extends BaseApplicationGenerator {
   get [BaseApplicationGenerator.INSTALL]() {
     return this.asInstallTaskGroup({
       async install() {
+        if (this.options.skipInstall) return;
         try {
           if (this.env.sharedFs.get(this.destinationPath('package.json'))?.committed) {
-            await this.spawnCommand('npm', ['install']);
+            await this.spawn('npm', ['install']);
           }
         } catch (error) {
           this.log.error(`Error executing 'npm install', execute by yourself.`);
