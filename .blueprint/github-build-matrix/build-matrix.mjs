86c6628de2500c5293701d58933ce46540250c61
Marcelo Shima <marceloshima@gmail.com>
2024-06-06 10:12:46 -0300

    rework workflow (#1788)
    * add samplesFolder option to local blueprints.
    
    * use generate-sample at ios workflow
    
    * convert ChangelogDate to strings
    
    * Revert "convert ChangelogDate to strings"
    
    This reverts commit 9d65a8322d1d6a7710d0702a88544b4b8f334ddd.

--- .blueprint/github-build-matrix/build-matrix.mjs
+++ .blueprint/github-build-matrix/build-matrix.mjs
@@ -3,10 +3,15 @@ import { RECOMMENDED_JAVA_VERSION, RECOMMENDED_NODE_VERSION } from 'generator-jh
 import { fromMatrix } from 'generator-jhipster/testing';
 
 const defaultMatrix = {
-  os: ['ubuntu-latest'],
+  os: ['macos-14'],
   'node-version': [RECOMMENDED_NODE_VERSION],
   'java-version': [RECOMMENDED_JAVA_VERSION],
-  'default-environment': ['prod'],
+  'default-environment': ['dev'],
+};
+
+const oauth2Sample = {
+  os: 'macos-14',
+  'default-environment': 'prod',
 };
 
 export const buildMatrix = async samplesFolder => {
@@ -14,9 +19,9 @@ export const buildMatrix = async samplesFolder => {
   return {
     include: Object.values(
       fromMatrix({
-        ...defaultMatrix,
+        ...(samplesFolder === 'oauth2' ? oauth2Matrix : defaultMatrix),
         'sample-name': samples.filter(sample => !sample.includes('disabled')),
       }),
-    ),
+    ).map(sample => (sample['sample-name'].includes('oauth2') ? { ...sample, ...oauth2Sample } : sample)),
   };
 };
