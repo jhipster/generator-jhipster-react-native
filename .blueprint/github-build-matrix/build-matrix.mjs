60776ca88410e54876470188195f72892970b0ff
Marcelo Shima <marceloshima@gmail.com>
2024-10-10 08:35:28 -0300

    ignore oauth2 sample in iOS workflow (#1926)

--- .blueprint/github-build-matrix/build-matrix.mjs
+++ .blueprint/github-build-matrix/build-matrix.mjs
@@ -3,7 +3,7 @@ import { RECOMMENDED_JAVA_VERSION, RECOMMENDED_NODE_VERSION } from 'generator-jh
 import { fromMatrix } from 'generator-jhipster/testing';
 
 const defaultMatrix = {
-  os: ['macos-14'],
+  os: ['macos-15'],
   'node-version': [RECOMMENDED_NODE_VERSION],
   'java-version': [RECOMMENDED_JAVA_VERSION],
   'default-environment': ['dev'],
@@ -20,7 +20,7 @@ export const buildMatrix = async samplesFolder => {
     include: Object.values(
       fromMatrix({
         ...(samplesFolder === 'oauth2' ? oauth2Matrix : defaultMatrix),
-        'sample-name': samples.filter(sample => !sample.includes('disabled')),
+        'sample-name': samples.filter(sample => !sample.includes('disabled') && !sample.includes('oauth2')),
       }),
     ).map(sample => (sample['sample-name'].includes('oauth2') ? { ...sample, ...oauth2Sample } : sample)),
   };
